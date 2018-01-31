4dcfa2e898051974619a3df2394aefd44b80a17d
Mark Ayers <mark@philoserf.com>
2018-01-31 08:16:58 -0800

    Remove dep and add logger to clone-all (#3)
    * Various clean up changes
    
    - add a Makefile
    - add a README to each sub project
    - add clone-all as a WIP
    - refactor to remove some lint
    - remove webcheck (as I do not like sourcegraph checkup's dependencies)
    - update all .gitignore files
    
    * Add dep config
    
    * Rename auth to authn
    
    * Add doc.go at root
    
    * Various clean up changes
    
    - add a Makefile
    - add a README to each sub project
    - add clone-all as a WIP
    - refactor to remove some lint
    - remove webcheck (as I do not like sourcegraph checkup's dependencies)
    - update all .gitignore files
    
    * Rename auth to authn
    
    * Add logger and drop dep

--- clone-all/main.go
+++ clone-all/main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/shurcooL/githubql"
+	"go.uber.org/zap"
 	"golang.org/x/oauth2"
 )
 
-func init() {
-	log.SetPrefix("clone-all: ")
-}
-
 func main() {
+
+	logger := newLogger()
+	log := logger.Sugar()
+	defer func() {
+		if err := logger.Sync(); err != nil {
+			fmt.Printf("log sync error: %v", err)
+		}
+	}()
+
 	ctx := context.Background()
 	client := authn()
 
@@ -29,18 +34,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("    Login:", query.Viewer.Login)
-	fmt.Println("CreatedAt:", query.Viewer.CreatedAt)
+	fmt.Println("Login:", query.Viewer.Login)
+	fmt.Println("Date:", query.Viewer.CreatedAt)
 
-	log.Fatal("TODO: finish the base implementation")
+	log.Info("TODO: finish the base implementation")
+}
+
+func newLogger() *zap.Logger {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		fmt.Printf("log creation error: %v", err)
+	}
+	return logger
 }
 
 func authn() *githubql.Client {
-	src := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	return githubql.NewClient(
+		oauth2.NewClient(
+			context.Background(),
+			oauth2.StaticTokenSource(
+				&oauth2.Token{
+					AccessToken: os.Getenv("GITHUB_TOKEN"),
+				},
+			),
+		),
 	)
-
-	httpClient := oauth2.NewClient(context.Background(), src)
-
-	return githubql.NewClient(httpClient)
 }
